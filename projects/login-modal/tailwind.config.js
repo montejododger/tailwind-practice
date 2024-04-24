464c931c2909fda3583bbf96f7442f60a36262a9
Matthew Montejo <montejododger@yahoo.com>
2024-04-24 13:19:13 -0700

    login modal done

---
+++ projects/login-modal/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./*html'],
+    theme: {
+        fontFamily: {
+            sans: ['Mullish', 'sans-serif'],
+            mono: ['Noto Sans', 'monospace'],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
