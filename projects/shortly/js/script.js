921f2998ffe60c284c5d42227a53da365d9311af
Matthew Montejo <montejododger@yahoo.com>
2024-05-02 11:45:29 -0700

    feature boxs complete

--- projects/shortly/js/script.js
+++ projects/shortly/js/script.js
@@ -1,10 +1,34 @@
 const input = document.getElementById('link-input');
 const linkForm = document.getElementById('link-form');
-const errMSg = document.getElementById('err-msg');
+const errMsg = document.getElementById('err-msg');
 
 linkForm.addEventListener('submit', formSubmit);
 
+const isValidUrl = urlString => {
+    var urlPattern = new RegExp(
+        '^(https?:\\/\\/)?' + // validate protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+            '(\\#[-a-z\\d_]*)?$',
+        'i'
+    ); // validate fragment locator
+    return !!urlPattern.test(urlString);
+};
+
 function formSubmit(e) {
     e.preventDefault();
-}
 
+    if (input.value === '') {
+        errMsg.innerHTML = 'Please Enter Something';
+        input.classList.add('border-red');
+    } else if (!isValidUrl(input.value)) {
+        errMsg.innerHTML = 'Please Enter Valid Url';
+        input.classList.add('border-red');
+    } else {
+        errMsg.innerHTML = '';
+        input.classList.remove('border-red');
+        alert('Success');
+    }
+}
