5f3bc1629648d1573cb9f7ebea4cab4065233a79
Matthew Montejo <montejododger@yahoo.com>
2024-05-08 10:37:48 -0700

    main functions work and calculates age

--- projects/age-calculator/js/scripts.js
+++ projects/age-calculator/js/scripts.js
@@ -1,23 +1,39 @@
-const day = document.getElementById('day');
-const month = document.getElementById('month');
-const year = document.getElementById('year');
+const ageForm = document.getElementById('ageForm');
 const agebtn = document.getElementById('agebtn');
 
-agebtn.addEventListener('click', howOld);
+const yearsNum = document.getElementById('years-nums');
+const monthsNum = document.getElementById('months-nums');
+const daysNum = document.getElementById('days-nums');
+
+
+ageForm.addEventListener('submit', howOld);
 
 function howOld(e) {
-    e.preventDefault;
-
-    const dayVal = day.value;
-    const monthVal = month.value;
-    const yearVal = year.value;
+    e.preventDefault();
 
     const currentDate = new Date();
-    const birthdate = new Date(`${yearVal}-${monthVal}-${dayVal}`);
 
-    let ageYears = currentDate.getFullYear() - birthdate.getFullYear();
-    let ageMonths = currentDate.getMonth() - birthdate.getMonth();
-    let ageDays = currentDate.getDay() - birthdate.getDay();
+    const formData = new FormData(ageForm);
+
+    const day = formData.get('day');
+    const month = formData.get('month');
+    const year = formData.get('year');
+
+
+    const birthdate = new Date(`${year},${month},${day}`);
+
+
+    const birthYear = birthdate.getFullYear();
+    const birthMonth = birthdate.getMonth() + 1;
+    const birthDay = birthdate.getDate();
+
+    const currYear = currentDate.getFullYear();
+    const currMonth = currentDate.getMonth() + 1;
+    const currDay = currentDate.getDate();
+
+    let ageYears = currYear - birthYear;
+    let ageMonths = currMonth - birthMonth;
+    let ageDays = currDay - birthDay;
 
     // Adjust for negative months or days
     if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
@@ -31,7 +47,9 @@ function howOld(e) {
             0
         ).getDate();
         ageMonths--;
-	}
-
-	console.log(ageYears, ageMonths, ageDays);
+    }
+	// ageYears === "" ? "" : monthsNum.innerText = ageYears;
+	yearsNum.innerText = ageYears;
+	monthsNum.innerText = ageMonths
+	daysNum.innerText = ageDays;
 }
